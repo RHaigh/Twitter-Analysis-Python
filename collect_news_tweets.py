df01a617238a0c28a6b7753c8b600e79620adee2
Richard Haigh <45393669+RHaigh@users.noreply.github.com>
2020-06-14 11:40:50 +0100

    Update collect_news_tweets.py

--- collect_news_tweets.py
+++ collect_news_tweets.py
@@ -14,8 +14,35 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
-# Enter in a list of strings, each of which is the @username of a verified news provider account
-search = api.user_timeline(screen_name = 'BBCWorld', count = 100, include_rts = False) 
+# We've searched for hashtags but what about specific users? We can use the same code but with the user_timeline function instead
+tweets = tweepy.Cursor(api.user_timeline,
+              screen_name="BBCWorld",
+              lang="en",
+              include_rts = False,
+              since="2020-06-01").items(50)
+
+user_data = [[tweet.created_at, tweet.user.name, remove_characters(tweet.text)] for tweet in tweets]
+
+# We may now move this into a pandas dataframe again
+tweet_df = pd.DataFrame(data=user_data,
+                    columns=['Created At', 'User', 'Text'])
 
 
-WIP
+# So what if we wanted a dataframe with the latest tweets from a range of news sources? We will utilise a for loop
+users = ["cnn", "BBCWorld", "SkyNews", "telegraph", "rt_com", "guardiannews", "ajenglish", "financialtimes"] # Ensure your usernames are verified and correct
+df = []
+for i in users: # For each user > perform the user_timeline scrape function
+    tweets = tweepy.Cursor(api.user_timeline,
+                  screen_name=i,
+                  lang="en",
+                  include_rts = False,
+                  since="2020-06-01").items(10)
+
+    user_data = [[tweet.created_at, tweet.user.name, remove_characters(tweet.text)] for tweet in tweets]
+    tweet_df = pd.DataFrame(data=user_data,
+                    columns=['Created At', 'User', 'Text'])
+    df.append(tweet_df)
+    
+# The for loop creates a list of dataframes for each user, then we simply append them to each other using the concat() function
+complete_table = pd.concat(df, ignore_index=True)
+
