684448bdff0163c8433a8fa217df12fd389076a7
Richard Haigh <45393669+RHaigh@users.noreply.github.com>
2020-06-14 16:41:19 +0100

    Update collect_news_tweets.py

--- collect_news_tweets.py
+++ collect_news_tweets.py
@@ -3,18 +3,9 @@ import tweepy as tweepy
 import pandas as pd
 import re
 
-# Enter in twitter api details here
-consumer_key = '2LphmpVvZKjodaXA1LxuEwoXd'
-consumer_secret = 'zrKVohL2RlOOfUKqspWdwSe3AUUx6aOaHDLoBx3C8EcPt1vwJV'
-access_key= '1092147060936265729-kwwbyHp9PAfJkcsM7n6ZEai7wiYf2x'
-access_secret = '4cPHmvp2nxI2BiayXzuECIgG7PJqb0ogQiNRrPqx7s8no'
+# You should have worked through the collect_tweets.py file and have imported the necessary api keys and auth permissions
 
-# These credentials are passed to Tweepys open-standard authorization protocol letting it know that it is allowed to access your twitter developer account
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth)
-
-# We've searched for hashtags but what about specific users? We can use the same code but with the user_timeline function instead
+# We've searched for hashtags but what about specific users? We can use the same code but with the user_timeline() tweepy function instead
 tweets = tweepy.Cursor(api.user_timeline,
               screen_name="BBCWorld",
               lang="en",
@@ -31,7 +22,7 @@ tweet_df = pd.DataFrame(data=user_data,
 # So what if we wanted a dataframe with the latest tweets from a range of news sources? We will utilise a for loop
 users = ["cnn", "BBCWorld", "SkyNews", "telegraph", "rt_com", "guardiannews", "ajenglish", "financialtimes"] # Ensure your usernames are verified and correct
 df = []
-for i in users: # For each user > perform the user_timeline scrape function
+for i in users: # For each user -> perform the user_timeline scrape function
     tweets = tweepy.Cursor(api.user_timeline,
                   screen_name=i,
                   lang="en",
@@ -46,3 +37,4 @@ for i in users: # For each user > perform the user_timeline scrape function
 # The for loop creates a list of dataframes for each user, then we simply append them to each other using the concat() function
 complete_table = pd.concat(df, ignore_index=True)
 
+# You will be left with a dataframe showing the latest tweets from each news source, allowing you to perform further analysis such as sentiment or word count
