f64ad8cbd7677a5af2f76629dd6935f7051ccee9
Richard Haigh <45393669+RHaigh@users.noreply.github.com>
2020-06-14 18:57:41 +0100

    Update collect_tweets.py

--- collect_tweets.py
+++ collect_tweets.py
@@ -4,10 +4,10 @@ import pandas as pd
 import re
 
 # Enter in twitter api details here
-consumer_key = '2LphmpVvZKjodaXA1LxuEwoXd'
-consumer_secret = 'zrKVohL2RlOOfUKqspWdwSe3AUUx6aOaHDLoBx3C8EcPt1vwJV'
-access_key= '1092147060936265729-kwwbyHp9PAfJkcsM7n6ZEai7wiYf2x'
-access_secret = '4cPHmvp2nxI2BiayXzuECIgG7PJqb0ogQiNRrPqx7s8no'
+consumer_key = 'key'
+consumer_secret = 'key'
+access_key= 'key'
+access_secret = 'key'
 
 # These credentials are passed to Tweepys open-standard authorization protocol letting it know that it is allowed to access your twitter developer account
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
